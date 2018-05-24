78705ebd8ca00008bdba6e35ed9ae3e5dcc8c6bc
Tiago Hermano <tiagofhermano@gmail.com>
2018-05-24 16:02:41 -0300

    Usando dynamic actions no Redux

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -3,41 +3,47 @@ import { createStore } from 'redux';
 const store = createStore((state = { count: 0 }, action) => {
   switch (action.type) {
     case 'INCREMENT':
+      const incrementBy = typeof action.incrementBy === 'number' ? action.incrementBy : 1;
       return {
-        count: state.count + 1
+        count: state.count + incrementBy
       };
-      break;
     case 'DECREMENT':
+      const decrementBy = typeof action.decrementBy === 'number' ? action.decrementBy : 1;
       return {
-        count: state.count - 1
+        count: state.count - decrementBy
       };
-      break;
+    case 'SET':
+      return {
+        count: action.count
+      }
     case 'RESET':
       return {
         count: 0
       };
-      break;
     default:
       return state;
   }
 });
 
-console.log(store.getState());
-
-store.dispatch({
-  type: 'INCREMENT'
+store.subscribe(() => {
+  console.log(store.getState());
 });
 
-console.log(store.getState());
-
 store.dispatch({
-  type: 'INCREMENT'
+  type: 'INCREMENT',
+  incrementBy: 5
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'DECREMENT',
+  decrementBy: 10
+});
 
 store.dispatch({
   type: 'RESET'
 });
 
-console.log(store.getState());
+store.dispatch({
+  type: 'SET',
+  count: 101
+});
