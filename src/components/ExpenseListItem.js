64e83cf89e5fcc6289d618646fddfede955b90f5
Tiago Hermano <tiagofhermano@gmail.com>
2018-06-04 13:01:19 -0300

    Criado input que altera o state de filters despachando actions pela UI

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+const ExpenseListItem = ({ id, description, amount, createdAt, dispatch }) => (
   <div>
     <h3>{description}</h3>
     <p>{amount}</p>
     <p>{createdAt}</p>
+    <button onClick={() => {
+      dispatch(removeExpense({ id }))
+    }}>Remove</button>
   </div>
 );
 
-export default ExpenseListItem;
+export default connect()(ExpenseListItem);
