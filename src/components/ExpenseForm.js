5f91d06af74157156396a376d0257b2c2d682da0
Tiago Hermano <tiagofhermano@gmail.com>
2018-06-08 14:40:51 -0300

    Instalado moment e Datepicker(Airbnb) e setado component SingleDatePicker no form de criação de Expense

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import moment from 'moment';
+import { SingleDatePicker } from 'react-dates';
+import 'react-dates/lib/css/_datepicker.css';
 
 export default class ExpenseForm extends React.Component {
   state = {
     description: '',
     amount: '',
     note: '',
+    createdAt: moment(),
+    calendarFocused: false
   };
 
   onDescriptionChange = (e) => {
@@ -22,7 +27,15 @@ export default class ExpenseForm extends React.Component {
     if (amount.match(/^\d*(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
-  }
+  };
+
+  onDateChange = (createdAt) => {
+    this.setState(() => ({ createdAt }));
+  };
+
+  onFocusChange = ({ focused }) => {
+    this.setState(() => ({ calendarFocused: focused }));
+  };
 
   render() {
     return(
@@ -40,6 +53,14 @@ export default class ExpenseForm extends React.Component {
             placeholder="Amount"
             value={this.state.amount}
             onChange={this.onAmountChange}
+          />
+          <SingleDatePicker
+            date={this.state.createdAt}
+            onDateChange={this.onDateChange}
+            focused={this.state.calendarFocused}
+            onFocusChange={this.onFocusChange}
+            numberOfMonths={1}
+            isOutsideRange={() => false}
           />
           <textarea
             placeholder="Add a note for your expense(optional)"
