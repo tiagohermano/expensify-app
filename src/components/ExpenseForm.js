1a78498b236dc14f524612275b4ae2131f48ae8c
Tiago Hermano <tiagofhermano@gmail.com>
2018-06-09 01:23:21 -0300

    Mandando o objeto Expense para o component anterior(AddExpense) e adicionando expense através de action

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -9,7 +9,8 @@ export default class ExpenseForm extends React.Component {
     amount: '',
     note: '',
     createdAt: moment(),
-    calendarFocused: false
+    calendarFocused: false,
+    error: ''
   };
 
   onDescriptionChange = (e) => {
@@ -24,23 +25,42 @@ export default class ExpenseForm extends React.Component {
 
   onAmountChange = (e) => {
     const amount = e.target.value;
-    if (amount.match(/^\d*(\.\d{0,2})?$/)) {
+    if (!amount || amount.match(/^\d{1,}(\.\d{0,2})?$/)) {
       this.setState(() => ({ amount }));
     }
   };
 
   onDateChange = (createdAt) => {
-    this.setState(() => ({ createdAt }));
+    if (createdAt) {
+      this.setState(() => ({ createdAt }));
+    }
   };
 
   onFocusChange = ({ focused }) => {
     this.setState(() => ({ calendarFocused: focused }));
   };
 
+  onSubmit = (e) => {
+    e.preventDefault();
+
+    if (!this.state.description || !this.state.amount) {
+      this.setState(() => ({ error: 'Please provide description and amount' }));
+    } else {
+      this.setState(() => ({ error: '' }));
+      this.props.onSubmit({
+        description: this.state.description,
+        amount: parseFloat(this.state.amount, 10) * 100,
+        createdAt: this.state.createdAt.valueOf(),
+        note: this.state.note
+      })
+    }
+  };
+
   render() {
     return(
       <div>
-        <form>
+        { this.state.error && <p>{this.state.error}</p> }
+        <form onSubmit={this.onSubmit}>
           <input
             type="text"
             placeholder="Description"
