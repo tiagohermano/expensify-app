2846ce1ba76d6ea4ea50fd62570626f1a48911f7
Tiago Hermano <tiagofhermano@gmail.com>
2018-06-15 17:25:45 -0300

    Criado primeiro teste de render de component(Header) utilizando o recurso Snapshot

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,7 +7,6 @@ const Header = () => (
     <NavLink to="/" activeClassName="is-active" exact>Home</NavLink>
     <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
     <NavLink to="/edit" activeClassName="is-active">Edit Expense</NavLink>
-    <NavLink to="/help" activeClassName="is-active">Help</NavLink>
   </div>
 );
 
