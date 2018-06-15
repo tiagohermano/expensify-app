a81c0d772c540f3d5f76912a01b77d5a9595ac81
Tiago Hermano <tiagofhermano@gmail.com>
2018-06-15 00:43:13 -0300

    Criado testes de do selector expenses

--- src/tests/actions/expenses.test.js
+++ src/tests/actions/expenses.test.js
@@ -1,4 +1,4 @@
-import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
+ import { addExpense, editExpense, removeExpense } from '../../actions/expenses';
 
 test('should setup remove expense action object', () => {
   const action = removeExpense({ id: 'abc123' });
