98efa106eef356a0c6b36b3ae01b3d5799c0e457
Tiago Hermano <tiagofhermano@gmail.com>
2018-06-15 15:06:09 -0300

    Criado testes do reducer expenses

---
+++ src/tests/reducers/expenses.test.js
@@ -0,0 +1,61 @@
+import expensesReducer from '../../reducers/expenses';
+import expenses from '../fixtures/expenses';
+
+test('should set default state', () => {
+  const state = expensesReducer(undefined, { type: '@@INIT' });
+  expect(state).toEqual([]);
+});
+
+test('should remove expense by id', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '2'
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([expenses[0], expenses[2]]);
+});
+
+test('should not remove expense if id not found', () => {
+  const action = {
+    type: 'REMOVE_EXPENSE',
+    id: '-1'
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
+
+test('should add expense', () => {
+  const expense = {
+    id: '4',
+    description: 'Lunch',
+    note: '',
+    amount: 12000,
+    createdAt: 1000,
+  };
+  const action = {
+    type: 'ADD_EXPENSE',
+    expense
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual([...expenses, expense]);
+});
+
+test('should edit expense', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: expenses[0].id,
+    updates: { description: 'Coffee' }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state[0].description).toEqual('Coffee');
+});
+
+test('should not edit expense if id not found', () => {
+  const action = {
+    type: 'EDIT_EXPENSE',
+    id: '-1',
+    updates: { description: 'Coffee' }
+  };
+  const state = expensesReducer(expenses, action);
+  expect(state).toEqual(expenses);
+});
