e3b9bd09a6067f9e877e26f969ef097bfdbbd0c1
Tiago Hermano <tiagofhermano@gmail.com>
2018-06-22 12:11:01 -0300

    Configurado geração do bundle para produção com webpack

--- src/app.js
+++ src/app.js
@@ -12,20 +12,13 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 
-
 const store = configureStore();
-store.dispatch(addExpense({ description: 'Water Bill', amount: 100 }));
-store.dispatch(addExpense({ description: 'Gas Bill', amount: 200, createdAt: 1000 }));
-store.dispatch(addExpense({ description: 'Rent', amount: 108500 }));
-
-const visibleExpenses = getVisibleExpenses(store.getState().expenses, store.getState().filters);
-
-console.log(visibleExpenses);
+console.log('testing');
 
 const jsx = (
   <Provider store={store}>
     <AppRouter />
   </Provider>
-)
+);
 
 ReactDOM.render(jsx, document.getElementById('app'));
