a92a23c12908241037e20d4a966daec4ac15d5ca
Tiago Hermano <tiagofhermano@gmail.com>
2018-07-09 10:29:30 -0300

    fix no webpack.DefinePlugin

--- webpack.config.js
+++ webpack.config.js
@@ -47,9 +47,9 @@ module.exports = (env) => {
       CSSExtract,
       new webpack.DefinePlugin({
         'process.env.FIREBASE_API_KEY': JSON.stringify(process.env.FIREBASE_API_KEY),
-        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
-        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
-        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_AUTH_DOMAIN': JSON.stringify(process.env.FIREBASE_AUTH_DOMAIN),
+        'process.env.FIREBASE_DATABASE_URL': JSON.stringify(process.env.FIREBASE_DATABASE_URL),
+        'process.env.FIREBASE_PROJECT_ID': JSON.stringify(process.env.FIREBASE_PROJECT_ID),
         'process.env.FIREBASE_STORAGE_BUCKET': JSON.stringify(process.env.FIREBASE_STORAGE_BUCKET),
         'process.env.FIREBASE_MESSAGE_SENDER_ID': JSON.stringify(process.env.FIREBASE_MESSAGE_SENDER_ID)
       })
